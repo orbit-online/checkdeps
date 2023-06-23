d8f7cfed82b8d1c32e13f6de4b9df85e1fa53f32
Anders Ingemann <aim@orbit.online>
2023-06-23 14:17:04 +0200

    Initial import

---
+++ checkdeps.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+checkdeps() {
+  set -eo pipefail
+  shopt -s inherit_errexit
+  local pkgroot
+  pkgroot=$(upkg root "${BASH_SOURCE[0]}")
+  # shellcheck source=.upkg/orbit-online/records.sh/records.sh
+  source "$pkgroot/.upkg/orbit-online/records.sh/records.sh"
+
+  local deps=("$@") dep out ret=0
+  for dep in "${deps[@]}"; do
+    if ! out=$(type "$dep" 2>&1); then
+      error "Dependency %s not found: %s" "$dep" "$out"
+      ret=1
+    fi
+  done
+  return $ret
+}
+
+checkdeps "$@"
